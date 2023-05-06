e34d1bd2447e9483f3a7342bbc6058a6cbe52172
Hamid Hamrah <s362052@gmail.com>
2023-05-06 11:30:42 +0200

    Commented the Sr and the server side.

--- Application.py
+++ Application.py
@@ -7,8 +7,8 @@ import queue
 import random
 import time
 header_format = '!IIHH'
-# Function that creat packets
-def convert(total_bytes, format):
+# The function give the possibilty for the user to choose the format for the throughput. 
+def convert(total_bytes, format):# 
     output = ""
     if format == "Bytes":
         total_bytes = total_bytes
@@ -30,33 +30,42 @@ def convert(total_bytes, format):
         out = round(total_bytes, 2)
         output += str(out) + unit
     return output
+# Fucntion to calculate the bandwidth 
 def calculate_bandwidth(data_bytes, time_seconds):
     data_bits = data_bytes * 8 # Convert bytes to bits
     bandwidth_bps = data_bits / time_seconds # Bandwidth in bits per second
     bandwidth_mbps = bandwidth_bps / 1000000 # Convert to Megabits per second
     return bandwidth_mbps
+# Function that creat packets
+# Functio given in hand. 
 def create_packet(seq, ack, flags, win, data):
     header = pack(header_format, seq, ack, flags, win)
     packet = header + data
     return packet
+# Function given in hand
 def parse_flags(flags):
     syn = flags & (1 << 3)
     ack = flags & (1 << 2)
     fin = flags & (1 << 1)
     return syn, ack, fin
+# Function given in hand. 
 def parse_header(header):
     seq, ack, flags, win = unpack(header_format, header)
     return seq, ack, flags, win
+# Function that take in a socket and a pack and an adress to send to. 
 def send_packet(socket, packet, addr):
     socket.sendto(packet, addr)
+# We use this function for the packet we recive and paser the header and data out of the packets
 def recv_packet(socket, size=1472):
     msg, addr = socket.recvfrom(size)
     seq, ack, flags, win = parse_header(msg[:12])
     syn, ack_flag, fin = parse_flags(flags)
     return msg, addr, seq, ack, syn, ack_flag, fin
+# The function takes in a a socket and sequence number and an adresse to send a packet with ACK flag. 
 def send_ack(socket, seq, addr):
     ack_packet = create_packet(seq, seq+1, 4, 0, b'')
     send_packet(socket, ack_packet, addr)
+# The fucntio used in the client side to recive the ack. 
 def recv_ack(socket):
     try:
         msg, addr, seq, ack, syn, ack_flag, fin = recv_packet(socket)
@@ -66,7 +75,6 @@ def recv_ack(socket):
             return None, None
     except socket.timeout:
         return None, None            
-
 # The implimention for the stop and wait method!
 def stop_and_wait(args):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)# Make a UDP socket
@@ -174,33 +182,33 @@ def gbn_client(args):
     print(f"{args.ip}:{args.port}   0.0 - {elapsed_time:.1f}          {convert(data_bytes,args.Type)}       {calculate_bandwidth(data_bytes, elapsed_time):.2f} Mbps")
 # The implimentation for th Selective-Repeat
 def sr_client(args):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)# Making a socket
     print("-----------------------------------------------------------")
     print(f"Server will start sending data to {args.ip}: {args.port}")
     print("-----------------------------------------------------------")
-    start_time=time.time()
+    start_time=time.time()# Star a timer to calculate the bandiwth later. 
     client_socket.settimeout(5)  # Set socket timeout to 5 seconds
-    syn_packet = create_packet(0, 0, 8, 0, b'')
-    send_packet(client_socket, syn_packet, (args.ip, args.port))
+    syn_packet = create_packet(0, 0, 8, 0, b'')# Creating a packet with syn flag. 
+    send_packet(client_socket, syn_packet, (args.ip, args.port))#Sending the the packet with Syn flag. 
 
-    with open(args.file, "rb") as f:
+    with open(args.file, "rb") as f:# Opening the file in byte mode. 
         while True:
             try:
                 msg, server_addr, seq, ack, syn, ack_flag, fin = recv_packet(client_socket)
-
+                #Part of three way handshake. 
                 if ack_flag and ack == 1:
                     send_ack(client_socket, 0, server_addr)#Sending an ACK to complete the three way hand shake
                     break
-            except socket.timeout:
+            except socket.timeout:# If we didnt recv an ack for the Packet with SYN falg we send it again. 
                 print("Timeout waiting for SYN-ACK packet. Resending SYN packet.")
                 send_packet(client_socket, syn_packet, (args.ip, args.port))
-        base = 1
-        next_seq = 1
-        window_size = args.window_size
-        acked_packets = set()
-        pkt_buffer = queue.Queue()
-        total_send=b''
-        eof = False
+        base = 1# Ref:GBN
+        next_seq = 1#Ref: GBN
+        window_size = args.window_size#Ref:GBN
+        acked_packets = set()#We store the ack packets 
+        pkt_buffer = queue.Queue()# Ref:GBN
+        total_send=b''#Used to calculate the bandiwth. 
+        eof = False#Ref: GBN
         while not eof or not pkt_buffer.empty():
             while next_seq < base + window_size and not eof:
                 data = f.read(1460)
@@ -228,7 +236,7 @@ def sr_client(args):
                 print("Timeout waiting for ACKs. Resending unacknowledged packets.")
                 for i in range(pkt_buffer.qsize()):
                     seq, data_packet = pkt_buffer.queue[i]
-                    if seq not in acked_packets:
+                    if seq not in acked_packets:# Her is the main difference with GBN where we check if the missing ack is in the ack packets or not if not we esnd again, 
                         send_packet(client_socket, data_packet, (args.ip, args.port))
                         total_send+=data
                         print(f"Resent packet with file data (seq {seq}) to server.")
@@ -265,13 +273,13 @@ def server(args):
             send_ack(server_socket, 0, client_addr)
         elif fin:# If the server recvieve a packet with fin flag!
             print("Received packet with FIN flag.")#INFO
-            recvd_file.close()
+            recvd_file.close()#Closing the file we opend
             print("File transfer complete. File saved as", args.file)#INFO
             break
         elif ack:# This is for the thre way handshake 
             continue
-        if args.reliability =='gbn':
-                if seq == base:
+        if args.reliability =='gbn':#If GBN is invokde
+                if seq == base:#If everthing is fine. 
                     if seq==skipnr and not skip_ack :
                         print(f"Skiping the ack for {seq}")
                         skip_ack=True
@@ -287,21 +295,21 @@ def server(args):
                 else:
                     print(f"Received out-of-order packet with seq {seq}. Discarding and not sending ACK.")
             
-        elif args.reliability == 'sr':
-            print(f"Received file data packet with packet {seq}.")
-            if seq == base:
-                if seq==skipnr and not skip_ack:
-                    print(f"skipping ack for packet{seq} ")
-                    skip_ack=True  
+        elif args.reliability == 'sr':# If sr is invoked
+            print(f"Received file data packet with packet {seq}.")#INFO
+            if seq == base:# Check if the packet is comming thr right order or not. 
+                if seq==skipnr and not skip_ack:# if we want to skip an ACK for a packet. 
+                    print(f"skipping ack for packet{seq} ")#INFO
+                    skip_ack=True  # It to skip one ack and thats it. 
                     continue
                 else:
-                    recvd_file.write(msg[12:])
-                    base += 1
-                    while base in out_of_order_buffer:
-                        recvd_file.write(out_of_order_buffer.pop(base))
+                    recvd_file.write(msg[12:])# If packets comes in righ order. 
+                    base += 1#Updating. 
+                    while base in out_of_order_buffer:# check if the base is in out of order list. 
+                        recvd_file.write(out_of_order_buffer.pop(base))# if yes then we pop it out and write to the file. 
                         base += 1
             else:
-                out_of_order_buffer[seq] = msg[12:]
+                out_of_order_buffer[seq] = msg[12:]# If its out of order we just put it into a list to store it when it time comes. 
 
             ack_packet = create_packet(seq, base, 4, 0, b'')
             send_packet(server_socket, ack_packet, client_addr)
@@ -341,10 +349,13 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port',          default=3030,        type=int,             help='Server port number.')
     parser.add_argument("-r", "--reliability",   type=str,            choices=["stop_and_wait", "gbn", "sr"], default= 'stop_and_wait', help="Reliability function to use.")
     parser.add_argument("-t", "--test",          type=int,            default=-1,           help="Ignore for the ack with specified seq number")
-    parser.add_argument('-T', '--Type',          type=str,                default='MB',                  choices=['BYTES','KB', 'MB', 'GB'] ,   help='choose the format of data')
+    parser.add_argument('-T', '--Type',          type=str,            default='MB',                  choices=['BYTES','KB', 'MB', 'GB'] ,   help='choose the format of data')
     parser.add_argument('-w', '--window_size',   default=5, type=int, help="THe window size")
     args = parser.parse_args()
-    if args.client:
+    if args.client and args.server:
+        print("You cannot run both at the same time!")
+        exit()
+    elif args.client:
         client(args)
     elif args.server:
         server(args)
